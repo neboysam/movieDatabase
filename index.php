69ea71281bbb31f7ae654ab09fddce3dded714ed
micneli <monte.nemi@gmail.com>
2020-02-19 13:20:25 +0100

    added admin, a try

--- index.php
+++ index.php
@@ -10,14 +10,16 @@ $mainRouter->addRouteGET('/movies/add', "Movies.addMovie");
 $mainRouter->addRoutePOST('/movies/add', "Movies.insertNewMovie");
 
 // A list of all the movies with the update option
-$mainRouter->addRouteGET('/movies/updatemovies', "Movies.getAllMovies");
+$mainRouter->addRouteGET('/admin/updatemovies', "Movies.getAllMovies");
 
 // Updating each movie separately
 $mainRouter->addRouteGET('/movies/updatemovies/:id', "Movies.getMovie");
 $mainRouter->addRoutePOST('/movies/updatemovies/:id', "Movies.updateMovie");
 
-$mainRouter->addRouteGET('/movies', 'Movies.index');
 $mainRouter->addRouteGET('/artists', 'Artists.index');
+
+// Administration
+$mainRouter->addRouteGET('/admin', 'Admin.index');
 
 try {
     $mainRouter->startRouter();
